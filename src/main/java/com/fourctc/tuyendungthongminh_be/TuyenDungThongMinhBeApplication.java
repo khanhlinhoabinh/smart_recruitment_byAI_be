1ddf38d31bddfdcb9a60b4777a276b512ff05af6
khanhlinhoabinh <khanhlinhphamhb@gmail.com>
2025-10-31 11:23:18 +0700

    first commit

---
+++ src/main/java/com/fourctc/tuyendungthongminh_be/TuyenDungThongMinhBeApplication.java
@@ -0,0 +1,13 @@
+package com.fourctc.tuyendungthongminh_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuyenDungThongMinhBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuyenDungThongMinhBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fourctc/tuyendungthongminh_be/TuyenDungThongMinhBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourctc.tuyendungthongminh_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuyenDungThongMinhBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
