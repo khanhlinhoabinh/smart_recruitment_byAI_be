8be6bf34eb1a60b0ef0197593e4c6ac21003b15e
ttnguyen4 <darkintran2005@gmail.com>
2025-10-31 16:26:51 +0700

    Update TuyenDungThongMinhBeApplication.java

--- src/main/java/com/fourctc/tuyendungthongminh_be/TuyenDungThongMinhBeApplication.java
+++ src/main/java/com/fourctc/tuyendungthongminh_be/TuyenDungThongMinhBeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TuyenDungThongMinhBeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TuyenDungThongMinhBeApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TuyenDungThongMinhBeApplication.class, args);
+        System.out.println("🚀 IRSS API is running successfully!");
+    }
 }
