f4e9e6d6ac4b849533df519076ad9c108460f539
Linh. Pham Khanh - CMC Global TTS <pklinh3@cmcglobal.vn>
2025-11-04 16:23:32 +0700

    themcongtruycap

--- src/main/java/com/fourctc/tuyendungthongminh_be/controller/UserController.java
+++ src/main/java/com/fourctc/tuyendungthongminh_be/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
-
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/users")
 public class UserController {
