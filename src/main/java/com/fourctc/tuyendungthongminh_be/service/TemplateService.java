999507d16bced9509466d45e3cc96fcde5b81940
TienHuyyy <tienhuy692005@gmail.com>
2025-11-25 00:14:46 +0700

    them api sua xoa cho template

--- src/main/java/com/fourctc/tuyendungthongminh_be/service/TemplateService.java
+++ src/main/java/com/fourctc/tuyendungthongminh_be/service/TemplateService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TemplateService {
@@ -28,5 +29,23 @@ public class TemplateService {
     public List<TemplateDTO> getAll() {
         return templateRepository.findAll()
                 .stream().map(templateMapper::entityToDTO).toList();
+    }
+    public TemplateDTO updateTemplate(UUID id, TemplateDTO dto) {
+        Template existing = templateRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Template không tồn tại"));
+
+        existing.setName(dto.getName());
+        existing.setPreviewImage(dto.getPreviewImage());
+        existing.setHtmlLayout(dto.getHtmlLayout());
+
+        Template updated = templateRepository.save(existing);
+        return templateMapper.entityToDTO(updated);
+    }
+
+    public void deleteTemplate(UUID id) {
+        if (!templateRepository.existsById(id)) {
+            throw new RuntimeException("Template không tồn tại");
+        }
+        templateRepository.deleteById(id);
     }
 }
