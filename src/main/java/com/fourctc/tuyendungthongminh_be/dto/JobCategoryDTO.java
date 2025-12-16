58798d1d808dd93df6571bab3bbc302a602fb3f0
khanhlinhoabinh <khanhlinhphamhb@gmail.com>
2025-12-16 11:27:53 +0700

    them anh vao job category

--- src/main/java/com/fourctc/tuyendungthongminh_be/dto/JobCategoryDTO.java
+++ src/main/java/com/fourctc/tuyendungthongminh_be/dto/JobCategoryDTO.java
@@ -2,16 +2,21 @@ package com.fourctc.tuyendungthongminh_be.dto;
 
 import lombok.*;
 import java.util.UUID;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.web.multipart.MultipartFile;
+
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class JobCategoryDTO {
     private UUID categoryId;
     private String name;
     private String description;
     private boolean popular;
     private String createdBy;
+    private String iconUrl;                    // trả về cho frontend
+
+    // Trường này chỉ dùng khi upload (không lưu vào DB)
 }
